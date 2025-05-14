bb586a4bc5bf4fa63070f9a7c310e2aa8df9dcfe
D1monPokem0n <373305@niuitmo.ru>
2025-05-15 00:40:58 +0300

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express from 'express'
+import { applyTrpcToExpressApp } from './lib/trpc'
+import { trpcRouter } from './router'
+
+const expressApp = express()
+expressApp.use(cors())
+expressApp.get('/ping', (req, res) => {
+  res.send('pong')
+})
+applyTrpcToExpressApp(expressApp, trpcRouter)
+expressApp.listen(3000, () => {
+  console.info('Listening at http://localhost:3000')
+})
